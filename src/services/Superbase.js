39f2719e9da84229adbdf982b2856d832c3d573c
Praveen Sharma <54062284+sharmapraveenofficial@users.noreply.github.com>
2023-10-01 22:48:03 +0530

    Fix: Environment variable created

--- src/services/Superbase.js
+++ src/services/Superbase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://kingdkrcopbqsspdamul.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtpbmdka3Jjb3BicXNzcGRhbXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNDk3MDAsImV4cCI6MjAxMTcyNTcwMH0.7Cnpe_J5vnPPCzxrZcTnwlVukpan8jIzNvtsJFHzII8";
+export const supabaseUrl = process.env.REACT_APP_SUPERBASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPERBASE_KEY;
 const Suparbase = createClient(supabaseUrl, supabaseKey);
 
 export default Suparbase;
